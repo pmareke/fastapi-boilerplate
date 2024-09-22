5dc2b8b489548b745285d182d948231b6bb1751c
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-09-22 11:27:38 +0200

    feat: use FastAPI DI mechanism

--- src/delivery/api/v1/hello/hello_router.py
+++ src/delivery/api/v1/hello/hello_router.py
@@ -5,15 +5,21 @@ from src.use_cases.say_hello_command import (
     SayHelloCommandHandler,
 )
 from src.domain.command import CommandHandler
+from src.domain.hello_client import HelloClient
 from src.delivery.api.v1.hello.hello_response import HelloResponse
 from src.infrastructure.hello.hello_client import DummyHelloClient
 
 hello_router: APIRouter = APIRouter()
 
 
-async def say_hello_command_handler() -> CommandHandler:
-    hello_client = DummyHelloClient()
-    return SayHelloCommandHandler(hello_client)
+async def hello_client() -> DummyHelloClient:
+    return DummyHelloClient()
+
+
+async def say_hello_command_handler(
+    client: HelloClient = Depends(hello_client),
+) -> CommandHandler:
+    return SayHelloCommandHandler(client)
 
 
 @hello_router.get("/api/v1/hello/{name}", response_model=HelloResponse)
