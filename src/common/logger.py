21898201179f9f389170933447cee27613c03467
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-09-26 10:41:55 +0200

    feat: refactor logger class

--- src/common/logger.py
+++ src/common/logger.py
@@ -1,17 +1,17 @@
 import logging
+from logging import INFO
 
 from src.common.settings import settings
 
 
 def setup_logging() -> None:
-    logger = logging.getLogger(settings.logger_name)
-    logger.setLevel(logging.INFO)
-
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    ch.setLevel(INFO)
 
     format = "%(levelname)s: %(asctime)s - %(message)s"
     formatter = logging.Formatter(format)
-
     ch.setFormatter(formatter)
+
+    logger = logging.getLogger(settings.logger_name)
+    logger.setLevel(INFO)
     logger.addHandler(ch)
