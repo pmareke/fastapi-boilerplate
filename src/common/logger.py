218f86ba629e14ef2cbc3f8667839ac1be5c7851
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-09-26 10:34:23 +0200

    feat: use settings in the logger class

--- src/common/logger.py
+++ src/common/logger.py
@@ -1,12 +1,17 @@
 import logging
 
+from src.common.settings import settings
+
 
 def setup_logging() -> None:
-    logger = logging.getLogger("server")
+    logger = logging.getLogger(settings.logger_name)
     logger.setLevel(logging.INFO)
+
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
+
     format = "%(levelname)s: %(asctime)s - %(message)s"
     formatter = logging.Formatter(format)
+
     ch.setFormatter(formatter)
     logger.addHandler(ch)
