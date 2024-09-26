24b6b589797c577436e11143b9f9b168b551ca64
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-09-26 10:43:15 +0200

    feat: change logger variable name

--- src/common/logger.py
+++ src/common/logger.py
@@ -5,13 +5,13 @@ from src.common.settings import settings
 
 
 def setup_logging() -> None:
-    ch = logging.StreamHandler()
-    ch.setLevel(INFO)
+    handler = logging.StreamHandler()
+    handler.setLevel(INFO)
 
     format = "%(levelname)s: %(asctime)s - %(message)s"
     formatter = logging.Formatter(format)
-    ch.setFormatter(formatter)
+    handler.setFormatter(formatter)
 
     logger = logging.getLogger(settings.logger_name)
     logger.setLevel(INFO)
-    logger.addHandler(ch)
+    logger.addHandler(handler)
