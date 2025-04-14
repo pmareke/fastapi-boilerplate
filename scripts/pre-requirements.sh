0427175e778db00ca1546f745ec9d61d1f8378de
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2025-04-14 18:25:49 +0200

    feat: Add docker installation check to pre-requirements.sh

--- scripts/pre-requirements.sh
+++ scripts/pre-requirements.sh
@@ -8,4 +8,13 @@ function uv_is_not_installed() {
     return 0
 }
 
+function docker_is_not_installed() {
+    if ! command -v docker &> /dev/null; then
+        echo "docker is not installed. Please go to https://docs.docker.com/get-docker/."
+        return 1
+    fi
+    return 0
+}
+
 uv_is_not_installed
+docker_is_not_installed
